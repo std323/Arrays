ee00a24bb80f3f94c7d52b66f2fdec8582d9be1c
Галима <@DESKTOP-PGIEF0N>
2023-02-17 11:59:03 +0300

    SortDone

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -340,24 +340,29 @@ void Sort(char arr[], const int n)
 }
 void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS)
 {
+	int interations = 0;
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
 		{
-			for (int k = 0; k < ROWS; k++)
+			for (int k = i; k < ROWS; k++)
 			{
-				for (int l = 0; l < COLS; l++)
-
-					if (arr[i][j] < arr[k][l])
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				{
+					//arr[i][j] - выбранный элемент
+					//arr[k][l] - перебираемый элемент
+					interations++;
+					if (arr[k][l] < arr[i][j])
 					{
-						int buffer = arr[k][l];
-						arr[k][l] = arr[i][j];
-						arr[i][j] = buffer;
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
 					}
-
+				}
 			}
 		}
 	}
+	cout << "Массив отсортирован за " << interations << endl;
 }
 void Sort(double arr[ROWS][COLS], const int ROWS, const int COLS)
 {
