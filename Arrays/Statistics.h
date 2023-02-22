ce893ae3b9050a40c0904c3925e51d0b5db43e0d
Галима <@DESKTOP-PGIEF0N>
2023-02-22 21:40:13 +0300

    SeparationTemplated

---
+++ Arrays/Statistics.h
@@ -0,0 +1,21 @@
+#pragma once
+#include"Constants.h"
+
+template<typename T>T Sum(T arr[], const int n);
+int Sum(char arr[], const int n);
+template<typename T>T Sum(T arr[ROWS][COLS], const int ROWS, const int COLS);
+int Sum(char arr[ROWS][COLS], const int ROWS, const int COLS);
+
+
+template<typename T>double Avg(T arr[], const int n);
+const char* Avg(char arr[], const int n);
+template<typename T>double Avg(T arr[ROWS][COLS], const int ROWS, const int COLS);
+const char* Avg(char arr[ROWS][COLS], const int ROWS, const int COLS);
+
+
+template<typename T>T minValueIn(T arr[], const int n);
+template<typename T>T minValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
+
+
+template<typename T>T maxValueIn(T arr[], const int n);
+template<typename T>T maxValueIn(T arr[ROWS][COLS], const int ROWS, const int COLS);
